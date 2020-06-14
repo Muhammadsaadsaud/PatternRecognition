2d26535212765a145c06b8ab1aa85de4278f5ef6
muhammadsaadsaud <muhammadsaadsaud@gmail.com>
2020-06-14 03:51:26 +0200

    - final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,32 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from 'src/components/navbar/navbar.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { ProjectComponent } from 'src/components/project/project.component';
+import { AboutComponent } from 'src/components/about/about.component';
+import { HomeComponent } from 'src/components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProjectComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatCardModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component:  HomeComponent},
+      { path: 'project', component: ProjectComponent },
+      { path: 'about', component: AboutComponent },
+    ])
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
